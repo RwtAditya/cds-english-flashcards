a8d2d73db72aed235f26745e21fa162af9b1a3d9
unknown <117914221+RwtAditya@users.noreply.github.com>
2026-06-14 11:49:33 +0530

    Initial commit: CDS English Exam Flashcards App

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Catch-all route to serve SPA frontend
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
